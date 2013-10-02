c253e8065bc398d3f578b841a539ab11cc1cefd3
bigfleet <jim@jimvanfleet.com>
2013-10-02 15:42:33 -0400

    Creating only the desired specs, closes #6

--- authed_app.rb
+++ authed_app.rb
@@ -67,6 +67,13 @@ GITIGNORE
 environment <<-APP_GENERATORS
   config.generators do |g|
     g.orm :active_record
+    g.test_framework :rspec, 
+      :fixtures => true, 
+      :view_specs => false, 
+      :helper_specs => false, 
+      :routing_specs => false, 
+      :controller_specs => false, 
+      :request_specs => true
     g.fixture_replacement :factory_girl, :dir => "spec/factories"
   end
   config.time_zone = 'Eastern Time (US & Canada)'
