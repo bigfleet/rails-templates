26559527468d42c5f833d99e5bb3db510d501a45
Jim Van Fleet <jim@jimvanfleet.com>
2014-11-10 13:49:00 -0500

    Closes #17

--- authed_app.rb
+++ authed_app.rb
@@ -104,6 +104,27 @@ RSpec.configure do |config|
 end
 CAPYBARA
 
+file "spec/support/forgery.rb", <<-FORGERY
+FactoryGirl.define do
+
+  sequence :email do |n|
+    Forgery(:internet).email_address
+  end
+
+  sequence :name do |n|
+    Forgery(:name).full_name
+  end
+
+  sequence :first_name do |n|
+    Forgery(:name).first_name
+  end
+
+  sequence :last_name do |n|
+    Forgery(:name).last_name
+  end
+end
+FORGERY
+
 file "spec/support/focus.rb", <<-FOCUS
 RSpec.configure do |config|
   config.filter_run :focused => true
