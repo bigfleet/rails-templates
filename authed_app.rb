814b38b9576e9ac773b97717ebcad39bada74be7
Jim Van Fleet <jim@jimvanfleet.com>
2014-11-10 14:25:02 -0500

    Closes #19

--- authed_app.rb
+++ authed_app.rb
@@ -125,6 +125,38 @@ FactoryGirl.define do
 end
 FORGERY
 
+file "spec/support/as_user.rb", <<-AS_USER
+include Devise::TestHelpers
+
+# gives us the login_as(@user) method when request object is not present
+include Warden::Test::Helpers
+Warden.test_mode!
+
+# Will run the given code as the user passed in
+def as_user(user=nil, &block)
+  current_user = user || Factory.create(:user)
+  if request.present?
+    sign_in(current_user)
+  else
+    login_as(current_user, :scope => :user)
+  end
+  block.call if block.present?
+  return self
+end
+
+
+def as_visitor(user=nil, &block)
+  current_user = user || Factory.stub(:user)
+  if request.present?
+    sign_out(current_user)
+  else
+    logout(:user)
+  end
+  block.call if block.present?
+  return self
+end
+AS_USER
+
 file "spec/support/focus.rb", <<-FOCUS
 RSpec.configure do |config|
   config.filter_run :focused => true
