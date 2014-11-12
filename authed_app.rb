76ed022c85a8a21a1815a15120c00e420297e004
Jim Van Fleet <jim@jimvanfleet.com>
2014-11-12 11:59:15 -0500

    Adds user factory, passing validation spec, and useful capy feature spec

--- authed_app.rb
+++ authed_app.rb
@@ -269,6 +269,8 @@ DATABASE
 run "bundle install"
 
 generate "rspec:install"
+
+gsub_file 'spec/rails_helper.rb', "# Dir[Rails.root.join", "Dir[Rails.root.join"
 run "mkdir spec/features"
 run "bundle exec guard init"
 if is_windows
@@ -282,6 +284,52 @@ generate "devise:install"
 generate "devise User"
 generate "devise:views"
 generate "rolify Role User"
+
+file "spec/features/login_spec.rb", <<-LOGIN_SPEC
+require 'rails_helper'
+
+describe "Logging in", :type => :feature do
+  
+  let(:user){ create(:user) }
+
+  it "signs me in" do
+    visit new_user_session_path
+    within("#new_user") do
+      fill_in 'Email', :with => user.email
+      fill_in 'Password', :with => '12345678'
+      click_button 'Log in'
+    end
+    #save_and_open_page
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+end
+LOGIN_SPEC
+
+run "rm spec/models/user_spec.rb"
+file "spec/models/user_spec.rb", <<-USER_SPEC
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  context "validation" do
+    subject{ build(:user) }
+    it{ should be_valid }
+  end
+end
+USER_SPEC
+
+run 'rm spec/factories/users.rb'
+file "spec/factories/users.rb", <<-USERS
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    email
+    password "12345678"
+    password_confirmation "12345678"
+  end
+end
+USERS
 
 rake "db:migrate db:test:prepare"
 
